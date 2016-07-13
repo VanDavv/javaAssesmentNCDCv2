36ce83e6d833615edd8c1669125169816c3a4aac
VanDavv <lukpila29@gmail.com>
2016-07-14 00:36:55 +0200

    Initial commit

---
+++ src/main/java/vandavv/JavaAssesmentApplication.java
@@ -0,0 +1,12 @@
+package vandavv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAssesmentApplication.class, args);
+	}
+}
---
+++ src/main/java/vandavv/ServletInitializer.java
@@ -0,0 +1,13 @@
+package vandavv;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JavaAssesmentApplication.class);
+	}
+
+}
